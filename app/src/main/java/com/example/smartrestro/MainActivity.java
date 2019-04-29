b610e8367ca0bfdb88872de8c9aa4240300eea73
gpujw <me.ujwal054@gmail.com>
2019-04-29 22:40:57 +0545

    Second commit

--- app/src/main/java/com/example/smartrestro/MainActivity.java
+++ app/src/main/java/com/example/smartrestro/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.smartrestro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myRef.setValue("Hello, World!");
+    }
+    public void getLogin(View view){
+        Intent intent = new Intent(this,NavigationActivity.class);
+        startActivity(intent);
     }
 }
