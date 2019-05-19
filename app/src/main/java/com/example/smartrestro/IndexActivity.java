bef8a0fa480e04503e1ddff625f5c078a60e224a
gpujw <me.ujwal054@gmail.com>
2019-05-19 10:50:53 +0545

    Index Activity committed

--- app/src/main/java/com/example/smartrestro/IndexActivity.java
+++ app/src/main/java/com/example/smartrestro/IndexActivity.java
@@ -5,18 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class IndexActivity extends AppCompatActivity {
     TextView txt;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        txt = findViewById(R.id.textViewLogin);
+        txt = findViewById(R.id.textViewSmartRestro);
+        progressBar = findViewById(R.id.progressBarIndex);
+        progressBar.setVisibility(View.GONE);
         txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
                 Intent I = new Intent(IndexActivity.this, MainActivity.class);
                 startActivity(I);
             }
