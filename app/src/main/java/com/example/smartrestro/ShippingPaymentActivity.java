a8e4f088ca645106c56548af817daa6a5fcb90c1
gpujw <me.ujwal054@gmail.com>
2019-05-19 11:25:26 +0545

    Shipping and payment activity file committed

--- app/src/main/java/com/example/smartrestro/ShippingPaymentActivity.java
+++ app/src/main/java/com/example/smartrestro/ShippingPaymentActivity.java
@@ -45,7 +45,7 @@ public class ShippingPaymentActivity extends AppCompatActivity
     private void logout() {
         FirebaseAuth.getInstance().signOut();
         Toast.makeText(ShippingPaymentActivity.this, "Logged Out Successfully", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(ShippingPaymentActivity.this, MainActivity.class);
+        Intent intent = new Intent(ShippingPaymentActivity.this, IndexActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
